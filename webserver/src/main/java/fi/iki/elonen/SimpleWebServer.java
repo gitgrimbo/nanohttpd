458eab768adb2d618845ba4fdbba4c04582a8b0c
gitgrimbo <gitgrimbo@gmail.com>
2013-08-21 21:40:52 +0100

    Extracted serveSingleFile() method.

--- webserver/src/main/java/fi/iki/elonen/SimpleWebServer.java
+++ webserver/src/main/java/fi/iki/elonen/SimpleWebServer.java
@@ -155,82 +155,90 @@ public class SimpleWebServer extends NanoHTTPD {
 
         try {
             if (res == null) {
-                // Get MIME type from file name extension, if possible
-                String mime = null;
-                int dot = f.getCanonicalPath().lastIndexOf('.');
-                if (dot >= 0) {
-                    mime = MIME_TYPES.get(f.getCanonicalPath().substring(dot + 1).toLowerCase());
-                }
-                if (mime == null) {
-                    mime = NanoHTTPD.MIME_DEFAULT_BINARY;
-                }
-
-                // Calculate etag
-                String etag = Integer.toHexString((f.getAbsolutePath() + f.lastModified() + "" + f.length()).hashCode());
-
-                // Support (simple) skipping:
-                long startFrom = 0;
-                long endAt = -1;
-                String range = header.get("range");
-                if (range != null) {
-                    if (range.startsWith("bytes=")) {
-                        range = range.substring("bytes=".length());
-                        int minus = range.indexOf('-');
-                        try {
-                            if (minus > 0) {
-                                startFrom = Long.parseLong(range.substring(0, minus));
-                                endAt = Long.parseLong(range.substring(minus + 1));
-                            }
-                        } catch (NumberFormatException ignored) {
-                        }
-                    }
-                }
-
-                // Change return code and add Content-Range header when skipping is requested
-                long fileLen = f.length();
-                if (range != null && startFrom >= 0) {
-                    if (startFrom >= fileLen) {
-                        res = new Response(Response.Status.RANGE_NOT_SATISFIABLE, NanoHTTPD.MIME_PLAINTEXT, "");
-                        res.addHeader("Content-Range", "bytes 0-0/" + fileLen);
-                        res.addHeader("ETag", etag);
-                    } else {
-                        if (endAt < 0) {
-                            endAt = fileLen - 1;
-                        }
-                        long newLen = endAt - startFrom + 1;
-                        if (newLen < 0) {
-                            newLen = 0;
-                        }
-
-                        final long dataLen = newLen;
-                        FileInputStream fis = new FileInputStream(f) {
-                            @Override
-                            public int available() throws IOException {
-                                return (int) dataLen;
-                            }
-                        };
-                        fis.skip(startFrom);
-
-                        res = new Response(Response.Status.PARTIAL_CONTENT, mime, fis);
-                        res.addHeader("Content-Length", "" + dataLen);
-                        res.addHeader("Content-Range", "bytes " + startFrom + "-" + endAt + "/" + fileLen);
-                        res.addHeader("ETag", etag);
-                    }
-                } else {
-                    if (etag.equals(header.get("if-none-match")))
-                        res = new Response(Response.Status.NOT_MODIFIED, mime, "");
-                    else {
-                        res = new Response(Response.Status.OK, mime, new FileInputStream(f));
-                        res.addHeader("Content-Length", "" + fileLen);
-                        res.addHeader("ETag", etag);
-                    }
-                }
+                serveSingleFile(header, f);
             }
         } catch (IOException ioe) {
             res = new Response(Response.Status.FORBIDDEN, NanoHTTPD.MIME_PLAINTEXT, "FORBIDDEN: Reading file failed.");
         }
 
         res.addHeader("Accept-Ranges", "bytes"); // Announce that the file server accepts partial content requestes
+        return res;
+    }
+
+    private Response serveSingleFile(Map<String, String> header, File f) throws IOException {
+        Response res = null;
+
+        // Get MIME type from file name extension, if possible
+        String mime = null;
+        int dot = f.getCanonicalPath().lastIndexOf('.');
+        if (dot >= 0) {
+            mime = MIME_TYPES.get(f.getCanonicalPath().substring(dot + 1).toLowerCase());
+        }
+        if (mime == null) {
+            mime = NanoHTTPD.MIME_DEFAULT_BINARY;
+        }
+
+        // Calculate etag
+        String etag = Integer.toHexString((f.getAbsolutePath() + f.lastModified() + "" + f.length()).hashCode());
+
+        // Support (simple) skipping:
+        long startFrom = 0;
+        long endAt = -1;
+        String range = header.get("range");
+        if (range != null) {
+            if (range.startsWith("bytes=")) {
+                range = range.substring("bytes=".length());
+                int minus = range.indexOf('-');
+                try {
+                    if (minus > 0) {
+                        startFrom = Long.parseLong(range.substring(0, minus));
+                        endAt = Long.parseLong(range.substring(minus + 1));
+                    }
+                } catch (NumberFormatException ignored) {
+                }
+            }
+        }
+
+        // Change return code and add Content-Range header when skipping is requested
+        long fileLen = f.length();
+        if (range != null && startFrom >= 0) {
+            if (startFrom >= fileLen) {
+                res = new Response(Response.Status.RANGE_NOT_SATISFIABLE, NanoHTTPD.MIME_PLAINTEXT, "");
+                res.addHeader("Content-Range", "bytes 0-0/" + fileLen);
+                res.addHeader("ETag", etag);
+            } else {
+                if (endAt < 0) {
+                    endAt = fileLen - 1;
+                }
+                long newLen = endAt - startFrom + 1;
+                if (newLen < 0) {
+                    newLen = 0;
+                }
+
+                final long dataLen = newLen;
+                FileInputStream fis = new FileInputStream(f) {
+                    @Override
+                    public int available() throws IOException {
+                        return (int) dataLen;
+                    }
+                };
+                fis.skip(startFrom);
+
+                res = new Response(Response.Status.PARTIAL_CONTENT, mime, fis);
+                res.addHeader("Content-Length", "" + dataLen);
+                res.addHeader("Content-Range", "bytes " + startFrom + "-" + endAt + "/" + fileLen);
+                res.addHeader("ETag", etag);
+            }
+        } else {
+            if (etag.equals(header.get("if-none-match")))
+                res = new Response(Response.Status.NOT_MODIFIED, mime, "");
+            else {
+                res = new Response(Response.Status.OK, mime, new FileInputStream(f));
+                res.addHeader("Content-Length", "" + fileLen);
+                res.addHeader("ETag", etag);
+            }
+        }
+
         return res;
     }
 
